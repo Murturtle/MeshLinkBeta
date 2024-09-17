1c2de930c0d249a84e0b3a4e04ee72b974c17ff5
Murturtle <97775022+Murturtle@users.noreply.github.com>
2024-09-16 21:36:43 -0700

    Update liblogger.py

--- plugins/liblogger.py
+++ plugins/liblogger.py
@@ -1,11 +1,11 @@
 def info(any):
-    print("[INFO] "+any)
+    print("[INFO] "+str(any))
 
 def warn(any):
-    print("\x1b[31;49m[WARN] "+any+"\x1b[0m")
+    print("\x1b[31;49m[WARN] "+str(any)+"\x1b[0m")
 
 def infoimportant(any):
-    print("\x1b[33;49m[INFO] "+any+"\x1b[0m")
+    print("\x1b[33;49m[INFO] "+str(any)+"\x1b[0m")
 
 def infogreen(any):
-    print("\x1b[32;49m[INFO] "+any+"\x1b[0m")
+    print("\x1b[32;49m[INFO] "+str(any)+"\x1b[0m")
