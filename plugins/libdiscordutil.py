23ab41ee253ec291efa27540371674fc54aaeb1e
pdxlocations <benlipsey@gmail.com>
2026-01-03 09:13:23 -0800

    add discord replies

--- plugins/libdiscordutil.py
+++ plugins/libdiscordutil.py
@@ -1,5 +1,34 @@
 import asyncio
+import collections
 import plugins.libmesh as LibMesh
+
+_MAX_TRACKED_MESSAGES = 1000
+_packet_message_ids = collections.OrderedDict()
+
+def _safe_int(value):
+    try:
+        return int(value)
+    except (TypeError, ValueError):
+        return None
+
+def _track_message_id(channel_id, packet_id, message_id):
+    channel_id = _safe_int(channel_id)
+    packet_id = _safe_int(packet_id)
+    message_id = _safe_int(message_id)
+    if channel_id is None or packet_id is None or message_id is None:
+        return
+    key = (channel_id, packet_id)
+    _packet_message_ids[key] = message_id
+    _packet_message_ids.move_to_end(key)
+    while len(_packet_message_ids) > _MAX_TRACKED_MESSAGES:
+        _packet_message_ids.popitem(last=False)
+
+def _lookup_message_id(channel_id, reply_id):
+    channel_id = _safe_int(channel_id)
+    reply_id = _safe_int(reply_id)
+    if channel_id is None or reply_id is None:
+        return None
+    return _packet_message_ids.get((channel_id, reply_id))
 
 def genUserName(interface, packet, details=True):
     short = LibMesh.getUserShort(interface, packet)
@@ -27,15 +56,33 @@ def genUserName(interface, packet, details=True):
 
     return ret
 
-def send_msg(message,client,config,channel_id=0):
+def send_msg(message,client,config,channel_id=0,packet_id=None,reply_id=None):
     if config["use_discord"]:
         if (client.is_ready()):
             if config.get("secondary_channel_message_ids") and channel_id and channel_id > 0:
-                chan = config["secondary_channel_message_ids"][channel_id-1]
-                asyncio.run_coroutine_threadsafe(client.get_channel(chan).send(message),client.loop)
+                channels = [config["secondary_channel_message_ids"][channel_id-1]]
             else:
-                for i in config["message_channel_ids"]:
-                    asyncio.run_coroutine_threadsafe(client.get_channel(i).send(message),client.loop)
+                channels = list(config["message_channel_ids"])
+
+            for chan_id in channels:
+                channel = client.get_channel(chan_id)
+                if channel is None:
+                    continue
+
+                async def _send_to_channel(ch, ch_id):
+                    target_id = _lookup_message_id(ch_id, reply_id)
+                    if target_id is not None:
+                        try:
+                            target = await ch.fetch_message(target_id)
+                            sent = await target.reply(message, mention_author=False)
+                        except Exception:
+                            sent = await ch.send(message)
+                    else:
+                        sent = await ch.send(message)
+
+                    _track_message_id(ch_id, packet_id, sent.id)
+
+                asyncio.run_coroutine_threadsafe(_send_to_channel(channel, chan_id), client.loop)
 
 def send_info(message,client,config):
     if config["use_discord"]:
